a0871472c3709d765f88228dcd91516e38bef333
Usuario <Usuario@DESKTOP-P3OGUBB>
2024-12-01 18:11:35 -0500

    Agregar archivos de proyecto.

---
+++ Context/MVC_Application_Context.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using MVC_Application.Models;
+
+namespace MVC_Application.Context
+{
+    public class MVC_Application_Context : DbContext
+    {
+    public MVC_Application_Context() : base ("name=MVC Project")
+        {
+        }
+
+    public DbSet<User> User { get; set; }
+
+    protected override void OnModelCreating(DbModelBuilder modelBuilder) //modificacion de metodos//
+    {
+            modelBuilder.Entity<User>()
+             .ToTable("Usuarios")
+            .HasKey(p => p.User_ID);
+            modelBuilder.Entity<User>()  //llamamos tabla y ponesmos atributos//
+                .Property(p => p.User_ID)
+                .IsRequired()
+            .HasColumnName("User_ID");
+
+            modelBuilder.Entity<User>()
+                .Property(p => p.Username)
+                .IsRequired()
+                .HasColumnName("Username")
+            .HasMaxLength(50);
+
+            modelBuilder.Entity<User>()
+                .Property(p => p.Password)
+                .IsRequired()
+                .HasColumnName("Password")
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<User>()
+                .Property(p => p.Creation_Date)
+                .IsRequired()
+                .HasColumnName("Creation_Date");
+
+            modelBuilder.Entity<User>()
+                .Property(p => p.HashKey)
+                .IsRequired()
+                .HasColumnName("HashKey");
+
+            modelBuilder.Entity<User>()
+                .Property(p => p.HashIV)
+                .IsRequired()
+                .HasColumnName("HashIV");
+
+
+            base.OnModelCreating(modelBuilder);
+    }
+}
+
+}
---
+++ Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Application.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int User_ID { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = " Este campo es obligatorio")]
+        [StringLength(50, ErrorMessage = "La cantidad max de caracteres es de 50")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [StringLength(50, ErrorMessage = "La cantidad max de caracteres es de 50")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public DateTime Creation_Date { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public DateTime HashKey { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public DateTime HashIV { get; set; }
+    }
+}
